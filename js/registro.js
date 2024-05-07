96cb4f8561124277e68082d2297a5be83585e61c
javier rosales <javier2aisc@gmail.com>
2024-05-07 08:09:41 -0600

    general

---
+++ js/registro.js
@@ -0,0 +1,36 @@
+document.getElementById("formulario").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const nombre= document.getElementById("nombre").value;
+    const appat = document.getElementById("appat").value;
+    const apmat = document.getElementById("apmat").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    var ajax = new XMLHttpRequest();
+    ajax.addEventListener("load", completo_handler, false);
+    ajax.addEventListener("error", error_handler, false);
+    ajax.addEventListener("abort", abort_handler, false);
+    ajax.open("POST", "http://localhost/sitioMuebleria/registro.php");
+
+    var paqueteRegistro = new FormData();
+    paqueteRegistro.append("nombre", nombre);
+    paqueteRegistro.append("appat",appat);
+    paqueteRegistro.append("apmat", apmat);
+    paqueteRegistro.append("email", email);
+    paqueteRegistro.append("password", password);
+
+    ajax.send(paqueteRegistro);
+    
+});
+function completo_handler(e) {
+    document.getElementById("status").innerHTML = e.target.response;
+}
+function error_handler(e) {
+    document.getElementById("status").innerHTML = e.target.response;
+
+}
+function abort_handler(e) {
+    document.getElementById("status").innerHTML = e.target.response;
+
+}
